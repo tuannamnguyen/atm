bc20b9e3756c583463e5e11c36d7d14577ac92df
tuannamnguyen <79750145+tuannamnguyen@users.noreply.github.com>
2021-05-19 15:27:31 +0700

    added create acc function

--- login.cpp
+++ login.cpp
@@ -1,20 +1,50 @@
 ﻿#include<iostream>
+#include<fstream>
+#include<vector>
+#include<map>
+#include<algorithm>
 using namespace std;
+void createAcc()
+{
+	ofstream id_out("id.txt", ios::app);
+	ofstream pass_out("pass.txt", ios::app);
+
+	string id, pass;
+	cout << "Xin vui long nhap so ID mong muon: ";
+	cin >> id;
+	id_out << id << endl;
+	cout << endl;
+
+	cout << "Xin vui long nhap mat khau mong muon: ";
+	cin >> pass;
+	pass_out << pass << endl;
+	cout << endl;
+}
 bool login()
 {
-	string id = "12345", id_in;
-	string pass = "54321", pass_in;
+	string id, id_from_file;
+	string pass, pass_from_file;
 	bool id_invalid = true;
 	bool pass_invalid = true;
 
+	ifstream id_fin("id.txt");
+	ifstream pass_fin("pass.txt");
+	vector<string> _id;
+	map<string, string> pass_data;
+	while (id_fin >> id_from_file)
+	{
+		_id.push_back(id_from_file);
+		pass_fin >> pass_from_file;
+		pass_data[id_from_file] = pass_from_file;
+	}
 
-	cout << "Xin chao!" << endl;
+
 
 	while (id_invalid)
 	{
 		cout << "Vui long nhap ID cua ban: ";
-		cin >> id_in;
-		if (id_in == id) id_invalid = false;
+		cin >> id;
+		if (find(_id.begin(), _id.end(), id) != _id.end()) id_invalid = false;
 		else cout << "So ID khong hop le. Xin vui long thu lai." << endl;
 	}
 
@@ -22,8 +52,8 @@ bool login()
 	while (pass_invalid)
 	{
 		cout << "Vui long nhap mat khau cua ban: ";
-		cin >> pass_in;
-		if (pass_in == pass) pass_invalid = false;
+		cin >> pass;
+		if (pass_data[id] == pass) pass_invalid = false;
 		else
 		{
 			c++;
@@ -33,7 +63,6 @@ bool login()
 				return false;
 			}
 			cout << "Ban da nhap sai mat khau. Xin vui long thu lai. Ban con " << 5 - c << " lan thu." << endl;
-			
 		}
 	}
 	return true;
